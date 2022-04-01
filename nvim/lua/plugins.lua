0b56283159457440342d9425e35b8ee3e630cc18
Lee Tang <tangli@lilith.com>
2022-04-01 17:03:25 +0800

    use nerdtree instead

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -101,12 +101,18 @@ return require('packer').startup(function(use)
         end,
     })
     use({
-        'kyazdani42/nvim-tree.lua',
-        requires = 'kyazdani42/nvim-web-devicons',
+        'preservim/nerdtree',
         config = function()
-            require('config.nvim-tree')
+            require('config.nerdtree')
         end,
     })
+    --use({
+    --'kyazdani42/nvim-tree.lua',
+    --requires = 'kyazdani42/nvim-web-devicons',
+    --config = function()
+    --require('config.nvim-tree')
+    --end,
+    --})
     use({
         'nvim-telescope/telescope.nvim',
         requires = 'nvim-lua/plenary.nvim',
